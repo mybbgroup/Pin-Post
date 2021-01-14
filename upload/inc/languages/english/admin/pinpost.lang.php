21599e3e995f78fa4f99554b6e17c36b6de6f2db
Omar Gonzalez <Sama34@users.noreply.github.com>
2021-01-13 16:36:44 -0800

    Update pinpost.lang.php

--- upload/inc/languages/english/admin/pinpost.lang.php
+++ upload/inc/languages/english/admin/pinpost.lang.php
@@ -9,3 +9,5 @@ $l['pinpost_limit_title'] = 'Pinnable Posts';
 $l['pinpost_limit_desc'] = 'Choose how many posts can be pinned for a thread.';
 $l['pinpost_author_title'] = 'Author Powered';
 $l['pinpost_author_desc'] = 'Allow the author of the thread to manage pin / unpin posts.';
+$l['pinpost_force_redirect_title'] = 'Force Redirect';
+$l['pinpost_force_redirect_desc'] = 'Force the redirect page when pinning or unpinning a post.';
